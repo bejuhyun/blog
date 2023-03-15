613ebe29494087c8d0b22a0e3dff48cc479d5e0e
bejuhyun <bejuhyun@gmail.com>
2023-03-15 15:58:58 +0900

    layout 구성

--- src/main/java/com/mori/blog/controller/BoardController.java
+++ src/main/java/com/mori/blog/controller/BoardController.java
@@ -2,13 +2,17 @@ package com.mori.blog.controller;
 
 import com.mori.blog.model.board;
 import com.mori.blog.repository.BoardRepository;
+import com.mori.blog.validator.BoardValidator;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
@@ -16,6 +20,9 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model){
@@ -25,10 +32,31 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model){
+    public String form(Model model, @RequestParam(required = false) Long id)
+    {
+        if (id == null){
+            board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", new board());
+        } else {
+            board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+//        if (id == null){
+//            model.addAttribute("board", new board());
+//        } else {
+//            board board = boardRepository.findById(id).orElse(null);
+//            model.addAttribute("board", board);
+        }
         return "board/form";
     }
 
-
-
+    @PostMapping("/form")
+    public String form(@Valid board board, BindingResult bindingResult)
+    {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
 }
--- src/main/java/com/mori/blog/model/board.java
+++ src/main/java/com/mori/blog/model/board.java
@@ -1,19 +1,30 @@
 package com.mori.blog.model;
 
+import com.sun.istack.NotNull;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+
 
 @Data
 @Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class board {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하")
     private String title;
     private String content;
 }
